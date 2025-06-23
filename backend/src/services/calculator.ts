013d6fbb3d2a3570292e7c4f713c528ed6ea653e
Marcos <mca88@alu.ua.es>
2025-06-23 17:03:48 +0200

    added 3 Periodos calculator,

--- backend/src/services/calculator.ts
+++ backend/src/services/calculator.ts
@@ -22,8 +22,8 @@ export function calculateTariff(days: DayConsumptionData[], tarif: Tariff, contr
         lowEnergy: { kwH: 0, price: 0 },
     }
     // Calculate Power
-    total.lowPower = Number((Math.round((contractedPower * days.length * tarif.power_prices.low) * 100) / 100).toFixed(2))
-    total.highPower = Number((Math.round((contractedPower * days.length * tarif.power_prices.high) * 100) / 100).toFixed(2))
+    total.lowPower = Number((Math.round((contractedPower * days.length * tarif.power_prices.low) * 100) / 100).toFixed(2));
+    total.highPower = Number((Math.round((contractedPower * days.length * tarif.power_prices.high) * 100) / 100).toFixed(2));
     total.totalPower = Number((total.lowPower + total.highPower).toFixed(2));
 
     // Calculate Energy
@@ -33,7 +33,7 @@ export function calculateTariff(days: DayConsumptionData[], tarif: Tariff, contr
 
         days.forEach(day => {
 
-            // Get night tariff hours threshold
+            // Get Noche tariff hours thresholds
             const monthN = day.Fecha.getMonth(); // 0-11
             const dayN = day.Fecha.getDate();  // 1-31
             const dateN = monthN * 100 + dayN;
@@ -49,29 +49,51 @@ export function calculateTariff(days: DayConsumptionData[], tarif: Tariff, contr
             day.Consumptions.forEach(consuption => {
                 if (consuption.Hora > expensiveHourStart && consuption.Hora <= expensiveHourEnd) {
                     total.highEnergy.price += (consuption.Consumo_kWh * tarif.energy_prices.high!);
-                    total.highEnergy.kwH += consuption.Consumo_kWh
+                    total.highEnergy.kwH += consuption.Consumo_kWh;
                 }
                 else {
                     total.lowEnergy.price += (consuption.Consumo_kWh * tarif.energy_prices.low!);
-                    total.lowEnergy.kwH += consuption.Consumo_kWh
+                    total.lowEnergy.kwH += consuption.Consumo_kWh;
                 }
-            })
+            });
         });
-        total.totalEnergy.kwH = total.lowEnergy.kwH + total.highEnergy.kwH
-        total.totalEnergy.price = total.lowEnergy.price + total.highEnergy.price
-        total.totalPrice = total.totalEnergy.price + total.totalPower
     }
     else if (tarif.type === TariffType.TresPeriodos) {
 
+        // Get 3 Periodos tariff hours thresholds
+        const LowRange: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
+        const MidRange: number[] = [9, 10, 15, 16, 17, 18, 23, 24];
+        const HighRange: number[] = [11, 12, 13, 14, 19, 20, 21, 22];
+
         days.forEach(day => {
+            let isWeekend = (day.Fecha.getDate() === 0 || day.Fecha.getDate() === 6);
 
-        })
-
-
+            // Start calculating price
+            day.Consumptions.forEach(consuption => {
+                if (isWeekend || LowRange.includes(consuption.Hora)) {
+                    total.lowEnergy.price += (consuption.Consumo_kWh * tarif.energy_prices.low!);
+                    total.lowEnergy.kwH += consuption.Consumo_kWh;
+                }
+                else if (MidRange.includes(consuption.Hora)) {
+                    total.midEnergy.price += (consuption.Consumo_kWh * tarif.energy_prices.mid!);
+                    total.midEnergy.kwH += consuption.Consumo_kWh;
+                }
+                else if (HighRange.includes(consuption.Hora)) {
+                    total.highEnergy.price += (consuption.Consumo_kWh * tarif.energy_prices.high!);
+                    total.highEnergy.kwH += consuption.Consumo_kWh;
+                }
+                else {
+                    console.log("-----NUNCA SE DEBE LLEGAR AQUÍ-----");
+                }
+            });
+        });
     }
     else if (tarif.type === TariffType.Inteligente) {
 
     }
+    total.totalEnergy.kwH = total.lowEnergy.kwH + total.midEnergy.kwH + total.highEnergy.kwH;
+    total.totalEnergy.price = total.lowEnergy.price + total.midEnergy.price + total.highEnergy.price;
+    total.totalPrice = total.totalEnergy.price + total.totalPower;
 
     return total
 }
--- backend/tests/tariffs.test.ts
+++ backend/tests/tariffs.test.ts
@@ -10,17 +10,17 @@ const contractedPower = 3.45;
 // Margins of error
 const priceError = 0.2;
 const kwHError = 1;
+//Tariffs
+const nocheTariff = allTariffs[0];
+const tresPeriodosTariff = allTariffs[1];
 
 describe('Noche tariff', () => {
-    const nightTariff = allTariffs[0];
-
     test('february-receipt', () => {
-        const content: string = fs.readFileSync(path.join(fixturesPath, 'consumo-febrero.csv'), 'utf8')
-        const consuptions = processCSV(content)
-        const grouped = groupConsuptionsIntoDays(consuptions)
+        const content: string = fs.readFileSync(path.join(fixturesPath, 'consumo-febrero.csv'), 'utf8');
+        const consuptions = processCSV(content);
+        const grouped = groupConsuptionsIntoDays(consuptions);
 
-        const result = calculateTariff(grouped, nightTariff, contractedPower);
-        console.log(result)
+        const result = calculateTariff(grouped, nocheTariff, contractedPower);
 
         //  Totals
         expect(Math.abs(result.totalPower - 9.68)).toBeLessThanOrEqual(priceError);
@@ -38,19 +38,18 @@ describe('Noche tariff', () => {
         expect(Math.abs(result.highEnergy.kwH - 40.86)).toBeLessThanOrEqual(kwHError);
         //    Mid
         expect(Math.abs(result.midEnergy.price - 0)).toBeLessThanOrEqual(priceError);
-        expect(Math.abs(result.midEnergy.kwH - 0)).toBeLessThanOrEqual(kwHError)
+        expect(Math.abs(result.midEnergy.kwH - 0)).toBeLessThanOrEqual(kwHError);
         //    Low
         expect(Math.abs(result.lowEnergy.price - 4.76)).toBeLessThanOrEqual(priceError);
-        expect(Math.abs(result.lowEnergy.kwH - 41.14)).toBeLessThanOrEqual(kwHError)
+        expect(Math.abs(result.lowEnergy.kwH - 41.14)).toBeLessThanOrEqual(kwHError);
 
     })
     test('march-receipt', () => {
-        const content: string = fs.readFileSync(path.join(fixturesPath, 'consumo-marzo.csv'), 'utf8')
-        const consuptions = processCSV(content)
-        const grouped = groupConsuptionsIntoDays(consuptions)
+        const content: string = fs.readFileSync(path.join(fixturesPath, 'consumo-marzo.csv'), 'utf8');
+        const consuptions = processCSV(content);
+        const grouped = groupConsuptionsIntoDays(consuptions);
 
-        const result = calculateTariff(grouped, nightTariff, contractedPower);
-        console.log(result)
+        const result = calculateTariff(grouped, nocheTariff, contractedPower);
 
         //  Totals
         expect(Math.abs(result.totalPower - 9.68)).toBeLessThanOrEqual(priceError);
@@ -68,19 +67,18 @@ describe('Noche tariff', () => {
         expect(Math.abs(result.highEnergy.kwH - 65.79)).toBeLessThanOrEqual(kwHError);
         //    Mid
         expect(Math.abs(result.midEnergy.price - 0)).toBeLessThanOrEqual(priceError);
-        expect(Math.abs(result.midEnergy.kwH - 0)).toBeLessThanOrEqual(kwHError)
+        expect(Math.abs(result.midEnergy.kwH - 0)).toBeLessThanOrEqual(kwHError);
         //    Low
         expect(Math.abs(result.lowEnergy.price - 8.58)).toBeLessThanOrEqual(priceError);
-        expect(Math.abs(result.lowEnergy.kwH - 74.21)).toBeLessThanOrEqual(kwHError)
+        expect(Math.abs(result.lowEnergy.kwH - 74.21)).toBeLessThanOrEqual(kwHError);
 
     })
     test('april-receipt', () => {
-        const content: string = fs.readFileSync(path.join(fixturesPath, 'consumo-abril.csv'), 'utf8')
-        const consuptions = processCSV(content)
-        const grouped = groupConsuptionsIntoDays(consuptions)
+        const content: string = fs.readFileSync(path.join(fixturesPath, 'consumo-abril.csv'), 'utf8');
+        const consuptions = processCSV(content);
+        const grouped = groupConsuptionsIntoDays(consuptions);
 
-        const result = calculateTariff(grouped, nightTariff, contractedPower);
-        console.log(result)
+        const result = calculateTariff(grouped, nocheTariff, contractedPower);
 
         //  Totals
         expect(Math.abs(result.totalPower - (1.73 + 10.03))).toBeLessThanOrEqual(priceError);
@@ -98,18 +96,17 @@ describe('Noche tariff', () => {
         expect(Math.abs(result.highEnergy.kwH - (12.1 + 70.98))).toBeLessThanOrEqual(kwHError);
         //    Mid
         expect(Math.abs(result.midEnergy.price - 0)).toBeLessThanOrEqual(priceError);
-        expect(Math.abs(result.midEnergy.kwH - 0)).toBeLessThanOrEqual(kwHError)
+        expect(Math.abs(result.midEnergy.kwH - 0)).toBeLessThanOrEqual(kwHError);
         //    Low
         expect(Math.abs(result.lowEnergy.price - (1.41 + 5.31))).toBeLessThanOrEqual(priceError);
-        expect(Math.abs(result.lowEnergy.kwH - (12.18 + 45.74))).toBeLessThanOrEqual(kwHError)
+        expect(Math.abs(result.lowEnergy.kwH - (12.18 + 45.74))).toBeLessThanOrEqual(kwHError);
     })
     test('april-receipt', () => {
-        const content: string = fs.readFileSync(path.join(fixturesPath, 'consumo-mayo.csv'), 'utf8')
-        const consuptions = processCSV(content)
-        const grouped = groupConsuptionsIntoDays(consuptions)
+        const content: string = fs.readFileSync(path.join(fixturesPath, 'consumo-mayo.csv'), 'utf8');
+        const consuptions = processCSV(content);
+        const grouped = groupConsuptionsIntoDays(consuptions);
 
-        const result = calculateTariff(grouped, nightTariff, contractedPower);
-        console.log(result)
+        const result = calculateTariff(grouped, nocheTariff, contractedPower);
 
         //  Totals
         expect(Math.abs(result.totalPower - 10.03)).toBeLessThanOrEqual(priceError);
@@ -127,9 +124,21 @@ describe('Noche tariff', () => {
         expect(Math.abs(result.highEnergy.kwH - (20.81 + 42.26))).toBeLessThanOrEqual(kwHError);
         //    Mid
         expect(Math.abs(result.midEnergy.price - 0)).toBeLessThanOrEqual(priceError);
-        expect(Math.abs(result.midEnergy.kwH - 0)).toBeLessThanOrEqual(kwHError)
+        expect(Math.abs(result.midEnergy.kwH - 0)).toBeLessThanOrEqual(kwHError);
         //    Low
         expect(Math.abs(result.lowEnergy.price - (2.09 + 3.83))).toBeLessThanOrEqual(priceError);
-        expect(Math.abs(result.lowEnergy.kwH - (17.96 + 32.97))).toBeLessThanOrEqual(kwHError)
+        expect(Math.abs(result.lowEnergy.kwH - (17.96 + 32.97))).toBeLessThanOrEqual(kwHError);
+    })
+})
+
+describe('3 Periodos Tariff', () => {
+
+    test('comparaciones', () => {
+        const content: string = fs.readFileSync(path.join(fixturesPath, 'consumo-abril.csv'), 'utf8');
+        const consuptions = processCSV(content);
+        const grouped = groupConsuptionsIntoDays(consuptions);
+
+        const result3Periodos = calculateTariff(grouped, tresPeriodosTariff, contractedPower);
+        // TO - DO hand made tests
     })
 })
