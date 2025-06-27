eed86e64eab282721af9d908bd8762dc0e58779a
Marcos Cerdán Amat <marcos.cerdan@bumerania.com>
2025-06-27 11:00:13 +0200

    added cors to fastify

---
+++ backend/src/routes/users.ts
@@ -0,0 +1,21 @@
+import fastify, { FastifyPluginAsync } from 'fastify';
+import { db } from '../firebase/firebase';
+
+const userRoutes: FastifyPluginAsync = async (fastify) => {
+    fastify.get('/login', async (request, reply) => {
+        if (!request.user) return reply.status(401).send({ error: 'Unauthorized' });
+
+        const userDoc = await db.collection('users').doc(request.user.uid).get();
+
+        if (!userDoc.exists) {
+            await db.collection('users').doc(request.user.uid).set({
+                email: request.user.email,
+                createdAt: new Date(),
+            });
+            return reply.code(201).send({ message: 'Usuario creado', new: true });
+        }
+        return reply.code(200).send({ message: 'Usuario ya existe', new: false });
+    });
+}
+
+export default userRoutes;
